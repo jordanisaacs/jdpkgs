e1508b91a25376dc400323803a1c709866654c10
Jordan Isaacs <mail@jdisaacs.com>
2022-07-15 23:49:55 -0700

    fix dolphin wrapper

--- pkgs/dolphin.nix
+++ pkgs/dolphin.nix
@@ -9,9 +9,11 @@ runCommand "dolphinWrapper"
 }
 ''
   mkdir $out
-  ln -s ${dolphin}/* $out
+  cp -Rs ${dolphin}/* $out
 
   # Remove kde requirement for starting dbus
-  head -n -1 $out/share/dbus-1/services/org.kde.dolphin.FileManager1.service > \
-    $out/share/dbus-1/services/org.kde.dolphin.FileManager1.service
+  chmod 755 $out/share/dbus-1/services/
+  rm $out/share/dbus-1/services/org.kde.dolphin.FileManager1.service
+  head -n -1 ${dolphin}/share/dbus-1/services/org.kde.dolphin.FileManager1.service \
+    > $out/share/dbus-1/services/org.kde.dolphin.FileManager1.service
 ''
